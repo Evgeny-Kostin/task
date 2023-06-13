99214c735115fdadaf5d05f75ff9bac6593e3900
Evgeny_Kostin <evgese@mail.ru>
2023-06-14 01:02:31 +0800

    added task0 task1 task3 task5

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда 
+//5 -> Пятница/
+
+Console.WriteLine("Input number:");
+int number1 = int.Parse(Console.ReadLine()!); 
+
+if (number1 == 3)
+    {
+        Console.WriteLine("->Wednesday");
+    }
+    else if (number1 == 5 )
+    {
+        Console.WriteLine("->Friday");
+    }
+    else
+        {
+            Console.WriteLine("Unknown day!");
+        }
