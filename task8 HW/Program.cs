4a30270cbd8c317ea2eca60e303fc88485889425
Evgeny_Kostin <evgese@mail.ru>
2023-06-14 22:36:03 +0800

    task6 HW Задача№6

---
+++ task8 HW/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №8: Напишите программу, котрая на вход принимает число N, 
+// а на выходе показывает все четные числа от 1 до N.
+// 5-> 2,4
+// 8-> 2,4,6,8
+
+Console.Write("Введите число N: "); 
+int N = int.Parse(Console.ReadLine()!); 
+if ((N % 2) == 0)
+{
+    Console.WriteLine("chentoe");
+}
+else
+{
+    Console.WriteLine("nechentoe");
+}
