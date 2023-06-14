50fc0d54174e13d6e1b64a145c7c32850adbd2e3
Evgeny_Kostin <evgese@mail.ru>
2023-06-14 10:05:08 +0800

    задача3

--- task3/Program.cs
+++ task3/Program.cs
@@ -2,18 +2,42 @@
 //3 -> Среда 
 //5 -> Пятница/
 
-Console.WriteLine("Input number:");
+Console.Write("Введите номер: ");
 int number1 = int.Parse(Console.ReadLine()!); 
 
 if (number1 == 3)
     {
-        Console.WriteLine("->Wednesday");
+        Console.Write("Среда");
     }
     else if (number1 == 5 )
     {
-        Console.WriteLine("->Friday");
+        Console.Write("Пятница");
     }
-    else
+    else if (number1 == 1)
         {
-            Console.WriteLine("Unknown day!");
+            Console.Write("Понедельник");
+        
+        }
+        else if (number1 == 2 )
+    {
+        Console.Write("Вторник");
+    }
+    else if (number1 == 4)
+        {
+            Console.Write("Четверг");
+        
+        }
+        else if (number1 == 6)
+        {
+            Console.Write("Суббота");
+        
+        }
+        else if (number1 == 7)
+        {
+            Console.Write("Воскресенье");
+        
+        }
+        else if (number1 >= 8)
+        {
+            Console.Write("Ведите число от 1 до 7");
         }
