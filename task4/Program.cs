eb1ad61e6f5be46b4507cc5fdd6698ec9a0f6ed3
Evgeny_Kostin <evgese@mail.ru>
2023-06-14 20:01:33 +0800

    task4 Домашнее задание задача№4

---
+++ task4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая на вход принимает три числа и выдает, 
+//максимальое из этих чисел.
+// 2 3 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.WriteLine("Введите числa A B C: ");
+
+int numberA = int.Parse(Console.ReadLine()!); 
+int numberB = int.Parse(Console.ReadLine()!);
+int numberC = int.Parse(Console.ReadLine()!);
+
+int max = numberA;
+
+if (numberA>max) {
+    Console.WriteLine(max=numberA);
+}
+if (numberB>max)
+{
+    Console.WriteLine(max=numberB);
+}
+if (numberC>max) {
+   Console.WriteLine(max=numberC);
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
