e18cc07996520ec72d09e8712ccbfa53176a432e
Evgeny_Kostin <evgese@mail.ru>
2023-06-20 01:15:01 +0800

    ДЗ задача№13 не математическое

---
+++ task13 HW/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача№13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78  -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+Console.WriteLine(stringNumber[2]);
+
