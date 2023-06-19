3e444cbe3a5e3d4b3cea70dbfd2ed6f2dec522fc
Evgeny_Kostin <evgese@mail.ru>
2023-06-19 23:05:08 +0800

    втрой вар 14б поправить

---
+++ task 14b/Program.cs
@@ -0,0 +1,17 @@
+﻿// напишите программу которая на вход принимает 2 числа и проверяет, является ли одно число квадратом другого.
+// 5, 25-> да, -4, 16 -> да, 25, 5 -> да, 8, 9 -> нет.
+
+Console.Clear();
+Console.WriteLine("Введите число1: ");
+int a = Convert.TwoInt32(Console.ReadLine());
+Console.WriteLine("Введите число2: ");
+int b = Convert.TwoInt32(Console.ReadLine());
+string res = (" ");
+if (b*b !=a && a*a != b)
+{
+    res = ("нет");
+}
+else{
+    res = ("да");
+}
+Console.WriteLine(res);
--- task14/Program.cs
+++ task14/Program.cs
@@ -1,14 +1,15 @@
 ﻿// напишите программу которая на вход принимает 2 числа и проверяет, является ли одно число квадратом другого.
 // 5, 25-> да, -4, 16 -> да, 25, 5 -> да, 8, 9 -> нет.
+
 Console.Clear();
 Console.Write("Введите число1: ");
 int a = int.Parse(Console.ReadLine()!);
 Console.Write("Введите число2: ");
 int b = int.Parse(Console.ReadLine()!);
-int x = 0;
-if (a>b)
+int x = 0; // квадрат меньшего числа
+if (a>b) 
 {
-    x = b * b;
+    x = b * b;  
     if (x==a)
     {
         Console.WriteLine($" {a}, {b} -> да");
