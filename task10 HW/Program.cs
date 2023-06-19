ff05aa25aeec735710c3aaf277a968c039fd3e85
Evgeny_Kostin <evgese@mail.ru>
2023-06-19 23:25:58 +0800

    ДЗ задача№10

---
+++ task10 HW/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача№10. Напишите программу, которая принимает 3-х значное число и на выходе показывает 
+//вторую цифру этого числа
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите трех значное число от 100 до 999: ");
+int num = int.Parse(Console.ReadLine()!);
+int firstValue = num/10%10;
+//int secondValue = num%10;
+Console.WriteLine(firstValue);
