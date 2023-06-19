38831e6ada21b31e88b22b4907908ea5842fb2a7
Evgeny_Kostin <evgese@mail.ru>
2023-06-19 20:29:35 +0800

    задача 13 проверка дерения на 2 числа одновременно

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13. Напишите программу которая на вход принимает число и проверяет кратно ли оно одновременно 7 и 23.
+// 14 -> no, 46-> no, 161->yes
+Console.Clear();
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!); // new Random().Next(100, 1000); 
+//int result = 0;
+//int result1 = number/7;
+//int result2 = number/23;
+if (number%7 == 0 && number%23 == 0) // чтобы одновременно сравнить с 2-я условиями ставим && условия приравниваем к 0. /не работатет
+{
+    Console.WriteLine(number + "-> да");
+    
+}
+else
+{
+    Console.WriteLine(number + "-> нет");
+}
