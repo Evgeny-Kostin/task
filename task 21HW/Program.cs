24cbea05d487ed570af4223a2ac776397d35da61
Evgeny_Kostin <evgese@mail.ru>
2023-06-22 17:03:56 +0800

    ДЗ 21 решение

--- task 21HW/Program.cs
+++ task 21HW/Program.cs
@@ -2,42 +2,22 @@
 //и находит расстояние между ними в 3D пространстве.
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
-// координаты x y z A(3,6,8)
+// координаты x y z   A(3,6,8)
 
-//int x1 = Coordinate("x", "A");
-//int y1 = Coordinate("y", "A");
-//int z1 = Coordinate("z", "A");
-//int x2 = Coordinate("x", "B");
-//int y2 = Coordinate("y", "B");
-//int z2 = Coordinate("z", "B");
+int numA1 = Prompt ("Введите координату Ax1: ");
+int numA2 = Prompt ("Введите координату Ay1: ");
+int numA3 = Prompt ("Введите координату Az1: ");
+int numB1 = Prompt ("Введите координату Bx2: ");
+int numB2 = Prompt ("Введите координату By2: ");
+int numB3 = Prompt ("Введите координату Bz2: ");
 
-//int Coordinate (string coordName, string pointName);
-//{
-    //int num = int.Parse(Console.ReadLine()!); 
-    //Console.WriteLine($"Координата {coordName} точки {pointName}");     
-    //return num; 
-//}
-
-
-
-int numberA1 = Prompt ("Введите координату А1: ");
-int numberA2 = Prompt ("Введите координату А2: ");
-int numberA3 = Prompt ("Введите координату А3: ");
-int numberB1 = Prompt ("Введите координату B1: ");
-int numberB2 = Prompt ("Введите координату B2: ");
-int numberB3 = Prompt ("Введите координату B3: ");
-
-//DistanceBetweenPoints(numberA1);
-//DistanceBetweenPoints(numberA2); 
-//DistanceBetweenPoints(numberA3);                 
-
-int Prompt (string message) 
+int Prompt (string message)
 {
     Console.WriteLine(message);
     int num = int.Parse(Console.ReadLine()!); 
     
     return num; 
 }
+double DistanceBetweenPoints = Math.Sqrt(Math.Pow((numB1-numA1), 2) + Math.Pow((numB2-numA2), 2) + Math.Pow((numB3-numA3), 2));
 
-
-Console.WriteLine($"A ({numberA1}, {numberA2}, {numberA3}); B ({numberB1}, {numberB2}, {numberB3}) ");
+Console.WriteLine($"A ({numA1}, {numA2}, {numA3}); B ({numB1}, {numB2}, {numB3} -> {DistanceBetweenPoints})");
