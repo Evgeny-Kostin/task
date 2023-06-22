cbca37923b2e5a74ffc9872353762cb6608b6783
Evgeny_Kostin <evgese@mail.ru>
2023-06-22 13:33:05 +0800

    задача 17

---
+++ task 21HW/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 21. Напишите программу, которая принимает на вход координаты двух точек
+//и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+// координаты x y z A(3,6,8)
+
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate (string coordName, string pointName);
+{
+    int num = int.Parse(Console.ReadLine()!); 
+    Console.WriteLine($"Координата {coordName} точки {pointName}");     
+    return num; 
+}
+
+
+
+//int numberA1 = Prompt ("Введите координату А1: ");
+//int numberA2 = Prompt ("Введите координату А2: ");
+//int numberA3 = Prompt ("Введите координату А3: ");
+
+//DistanceBetweenPoints(numberA1);
+//DistanceBetweenPoints(numberA2); 
+//DistanceBetweenPoints(numberA3);                  //прописанно универсально для всех намберов только вводи новый намбер
+
+//int Prompt (string message) //название любое ф-ция типа инт, приходит переменная типа стринг назвали месседж
+//{
+    //Console.Clear();
+    //Console.WriteLine(message);
+    //int num = int.Parse(Console.ReadLine()!); //int.Parse(Console.ReadLine()); Делает положительным входящее отрицательное значение
+      //если придет от пользователя отрицательное значение
+    //return num; // для того и создали промт , чтобы число вернуть
+//}
--- task13HW2/Program.cs
+++ task13HW2/Program.cs
@@ -4,13 +4,42 @@
 //32679 -> 6
 
 
-Console.Clear();
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());                 
-int secondValue = number%10;                                //находим второе число
+//Console.Clear();
+//Console.WriteLine("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());                 
+//int secondValue = number%10;                                //находим второе число
 //Console.WriteLine(secondValue + " -> Третье число"); 
-if (number<100) {
-    Console.WriteLine("Третьей цифры нет");
-}
-Console.WriteLine(secondValue + " -> Третье число");
+//if (number<100) {
+//    Console.WriteLine("Третьей цифры нет");
+//}
+//Console.WriteLine(secondValue + " -> Третье число");
 
+//объяснение преподавателем
+//string mess = "введите число: " можно и так тогда в инт промт mess
+int number = Prompt ("Введите число: "); //таким образом можно несколько чисел вставлять number1,2,3,4
+FindTherdNumber(number);                 //прописанно универсально для всех намберов только вводи новый намбер
+
+int Prompt (string message) //название любое ф-ция типа инт, приходит переменная типа стринг назвали месседж
+{
+    Console.Clear();
+    Console.WriteLine(message);
+    int num = Math.Abs(int.Parse(Console.ReadLine())); //int.Parse(Console.ReadLine()); Делает положительным входящее отрицательное значение
+      //если придет от пользователя отрицательное значение
+    return num; // для того и создали промт , чтобы число вернуть
+}
+void FindTherdNumber (int num) // название - отражение сути действия, тип войд -возвращать ничего не будет
+{
+    if(num<100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        while (num>=1000) // если больше 4 знаков, число уменьшаем до 3 чтобы оно не попало в цикл и перешло в инт ферднамбер%10
+        {
+            num = num/10;
+        }
+        int therdNumber = num%10; // нужно для того чтобы в цикл ваил не заходить если вводится трез хнач чило ведь цикл с 4 зн числа работает
+        Console.WriteLine($"Третья цифра {therdNumber}");
+    }
+}
