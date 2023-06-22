642c3527e730c627a15021a46c56fdf78354524f
Evgeny_Kostin <evgese@mail.ru>
2023-06-22 22:17:10 +0800

    ДЗ27

--- task 23HW/Program.cs
+++ task 23HW/Program.cs
@@ -5,16 +5,20 @@
 
 int number = Prompt ("Введите число: ");
 int i = 1;
+Cube(number);
 int Prompt (string message)
 {
     Console.WriteLine(message);
     int number = int.Parse(Console.ReadLine()!);
     return number;
 }
+
+void Cube(int number)
+{
 while(i<=number) 
 {
     
     Console.Write(i*i*i + " ");
     i++;
 }
-
+}
--- task 27HW/Program.cs
+++ task 27HW/Program.cs
@@ -3,5 +3,33 @@
 //82 -> 10
 //9012 -> 12
 
-Console.WriteLine("Введите число: ");
-int userNum = TakeUserNum ();
+int a = Promt ("Введите число: ");
+int len = Length(a);
+Console.Write($"Сумма цифр в числе {a} равно: {Sum(a,len)}");
+int Promt(string message) 
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+int Length(int a)
+{
+int index = 0;
+while (a > 0)
+{
+a /= 10;
+index++;
+}
+return index;
+}
+
+int Sum(int a, int len)
+{
+int sum = 0;
+for (int i = 1; i <= len; i++)
+{
+sum += a % 10;
+a /= 10;
+}
+return sum;
+}
