fe84746c7826a76f66cd140e71434f8de7507dd0
Evgeny_Kostin <evgese@mail.ru>
2023-06-22 15:12:09 +0800

    задача 18 и начало

--- task 21HW/Program.cs
+++ task 21HW/Program.cs
@@ -4,35 +4,40 @@
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 // координаты x y z A(3,6,8)
 
-int x1 = Coordinate("x", "A");
-int y1 = Coordinate("y", "A");
-int z1 = Coordinate("z", "A");
-int x2 = Coordinate("x", "B");
-int y2 = Coordinate("y", "B");
-int z2 = Coordinate("z", "B");
+//int x1 = Coordinate("x", "A");
+//int y1 = Coordinate("y", "A");
+//int z1 = Coordinate("z", "A");
+//int x2 = Coordinate("x", "B");
+//int y2 = Coordinate("y", "B");
+//int z2 = Coordinate("z", "B");
 
-int Coordinate (string coordName, string pointName);
+//int Coordinate (string coordName, string pointName);
+//{
+    //int num = int.Parse(Console.ReadLine()!); 
+    //Console.WriteLine($"Координата {coordName} точки {pointName}");     
+    //return num; 
+//}
+
+
+
+int numberA1 = Prompt ("Введите координату А1: ");
+int numberA2 = Prompt ("Введите координату А2: ");
+int numberA3 = Prompt ("Введите координату А3: ");
+int numberB1 = Prompt ("Введите координату B1: ");
+int numberB2 = Prompt ("Введите координату B2: ");
+int numberB3 = Prompt ("Введите координату B3: ");
+
+//DistanceBetweenPoints(numberA1);
+//DistanceBetweenPoints(numberA2); 
+//DistanceBetweenPoints(numberA3);                 
+
+int Prompt (string message) 
 {
+    Console.WriteLine(message);
     int num = int.Parse(Console.ReadLine()!); 
-    Console.WriteLine($"Координата {coordName} точки {pointName}");     
+    
     return num; 
 }
 
 
-
-//int numberA1 = Prompt ("Введите координату А1: ");
-//int numberA2 = Prompt ("Введите координату А2: ");
-//int numberA3 = Prompt ("Введите координату А3: ");
-
-//DistanceBetweenPoints(numberA1);
-//DistanceBetweenPoints(numberA2); 
-//DistanceBetweenPoints(numberA3);                  //прописанно универсально для всех намберов только вводи новый намбер
-
-//int Prompt (string message) //название любое ф-ция типа инт, приходит переменная типа стринг назвали месседж
-//{
-    //Console.Clear();
-    //Console.WriteLine(message);
-    //int num = int.Parse(Console.ReadLine()!); //int.Parse(Console.ReadLine()); Делает положительным входящее отрицательное значение
-      //если придет от пользователя отрицательное значение
-    //return num; // для того и создали промт , чтобы число вернуть
-//}
+Console.WriteLine($"A ({numberA1}, {numberA2}, {numberA3}); B ({numberB1}, {numberB2}, {numberB3}) ");
---
+++ task18/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 18. Напишите программу, которая по заданному номеру четверти,
+//показывет диапазон возможных координат точек в этой четверти. (x и y).
+
+
+int number = Prompt ("Введите номер четверти: ");
+
+FindNumberQuarter(number);
+
+void FindNumberQuarter (int number)
+{
+if(number == 1)
+{
+    Console.WriteLine("x>0, Y>0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("x<0, y>0ь");
+     
+}
+else if (number == 3)
+{
+    Console.WriteLine("x<0, y<0");
+}
+else if (number == 4)
+{
+    Console.WriteLine("x>0, y<0");
+}
+else //обработка ввода некорректного значения 1;0 или 0;0
+{
+    Console.WriteLine(" Ошибка");
+}
+}
+int Prompt (string message) 
+{         
+    Console.Write(message);
+    int number=int.Parse(Console.ReadLine()!); 
+    return number;
+}
