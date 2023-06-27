4f5e0cb301e0ac4022c09ea8113b9a2101000400
Evgeny_Kostin <evgese@mail.ru>
2023-06-27 16:48:49 +0800

    семинар6

---
+++ task6_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон. 
+// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+int a = TakeUserNum();
+int b = TakeUserNum();
+int c = TakeUserNum();
+
+if(TriangleInequality(a, b, c))
+    Console.WriteLine($"Треугольник со сторонами такой длины может существовать");
+else
+    Console.WriteLine($"Треугольник со сторонами такой длины НЕ может существовать");
+
+bool TriangleInequality(int a, int b, int c)
+{
+    bool inequality = true;
+
+    if(a >= (b + c) || b >= (a + c) || c >= (a + b)) inequality = false;
+
+    return inequality;
+}
+
+int TakeUserNum()
+{
+    int userNum;
+
+    while(!int.TryParse(Console.ReadLine(), out userNum))
+        Console.WriteLine("Пожалуйста введите число!");
+
+    return userNum;
+}
---
+++ task6_42/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+
+
+// int Prompt(string message)
+// {
+//     Console.WriteLine(message);
+//     int number = int.Parse(Console.ReadLine());
+//     return number;
+// }
+
+
+// void Transformation(int res)
+// {
+//     string result = string.Empty;
+//     while (res > 0)
+//     {
+
+//         int remainder = res % 2;
+//         res /= 2;
+//         result = remainder.ToString() + result;
+//     }
+
+//     Console.Write($"В двоичном коде число равно: " + result);
+// }
+
+// int number = Prompt("Введите число: ");
+// Transformation(number);
